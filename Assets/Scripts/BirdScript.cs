74b34c75f341e1665d4be9e81c46cd6c67845d37
Hilay <hilaywifi@gmail.com>
2025-09-03 12:48:50 +0300

    added top and bottom boundries

--- Assets/Scripts/BirdScript.cs
+++ Assets/Scripts/BirdScript.cs
@@ -7,14 +7,30 @@ public class BirdScript : MonoBehaviour
     [SerializeField] private GameManager gameManager;
     public bool isAlive = true;
 
+    private float topLimit;
+    private float bottomLimit;
+
     void Start()
     {
+        // Get camera bounds in world units
+        Camera cam = Camera.main;
+        float halfHeight = cam.orthographicSize;
+        float halfWidth = halfHeight * cam.aspect;
+
+        topLimit = cam.transform.position.y + halfHeight;
+        bottomLimit = cam.transform.position.y - halfHeight;
     }
 
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.Space) && isAlive){
             myRigidBody.linearVelocity = Vector2.up * jumpScaler;
+        }
+
+        if (isAlive && (transform.position.y > topLimit || transform.position.y < bottomLimit))
+        {
+            gameManager.GameOver();
+            isAlive = false;
         }
     }
 
